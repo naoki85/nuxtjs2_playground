620545707f0a9a27210c30faeba76e639f695213
naoki85 <naoki.yoneyama.85@gmail.com>
2019-04-06 16:49:21 +0900

    Add paginate compoment

---
+++ components/paginate.vue
@@ -0,0 +1,111 @@
+<template>
+  <div v-if="totalPage > 0" class="text-xs-center">
+    <ul class="paginate">
+      <li @click="emitClickPage(currentPage - 1)">
+        <i class="fas fa-chevron-left"></i>
+      </li>
+      <li
+        v-for="page in displayPages"
+        :key="page"
+        :class="isCurrent(page)"
+        @click="emitClickPage(page)"
+      >
+        {{ page }}
+      </li>
+      <li @click="emitClickPage(currentPage + 1)"><i class="fas fa-chevron-right"></i></li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    currentPage: {
+      type: Number,
+      default: 1
+    },
+    totalPage: {
+      type: Number,
+      default: 0
+    }
+  },
+  computed: {
+    displayPages: function() {
+      if (this.totalPage && this.totalPage <= 5) {
+        return this.totalPage
+      }
+      if (this.currentPage - 3 <= 0) {
+        return 5
+      }
+      const tmpPages = []
+      const iterater = [-2, -1, 0, 1, 2]
+      iterater.forEach(i => {
+        const tmpPage = this.currentPage + i
+        if (tmpPage > this.totalPage) {
+          return true
+        }
+        tmpPages.push(tmpPage)
+      })
+      return tmpPages
+    }
+  },
+  methods: {
+    isCurrent: function(page) {
+      const defaultClass = 'page-' + page
+      return page === this.currentPage
+        ? defaultClass + ' current'
+        : defaultClass
+    },
+    emitClickPage(page) {
+      this.$emit('click-page', page)
+    },
+    updateCurrent(page) {
+      const currentClassElements = document.getElementsByClassName('current')
+      if (currentClassElements.length > 0) {
+        currentClassElements[0].classList.remove('current')
+        document
+          .getElementsByClassName('page-' + page)[0]
+          .classList.add('current')
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+li {
+  list-style: none;
+  font-weight: bold;
+}
+.paginate {
+  display: inline-flex;
+  display: -webkit-inline-box;
+  display: -ms-inline-flexbox;
+  padding: 9px 8px;
+  -webkit-border-radius: 3px;
+  -moz-border-radius: 3px;
+  border-radius: 3px;
+}
+.paginate li {
+  display: block;
+  color: #5ea25a;
+  text-decoration: none;
+  padding: 9px 12px;
+  margin-right: 6px;
+  border-radius: 16px;
+  background: #fff;
+  -webkit-transition: all 0.3s linear;
+  -moz-transition: all 0.3s linear;
+  transition: all 0.3s linear;
+  box-shadow: 0 3px 1px -2px rgba(0, 0, 0, 0.2), 0 2px 2px 0 rgba(0, 0, 0, 0.14),
+    0 1px 5px 0 rgba(0, 0, 0, 0.12);
+}
+.paginate li:hover {
+  color: #4f664e;
+  background: #c9dec8;
+}
+.paginate li.current {
+  background: rgb(0, 209, 178);
+  color: #fff;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -8,6 +8,12 @@
       sm10
       md8
     >
+      <v-paginate
+        ref="paginate"
+        :current-page="page"
+        :total-page="totalPage"
+        @click-page="fetchPosts"
+      />
       <v-layout row wrap>
         <v-flex>
           <v-card v-for="(post, i) in posts" :key="i" :to="`/posts/${post.ID}`">
@@ -17,13 +23,17 @@
                   src="http://localhost:3000/v.png"
                   height="125px"
                   contain
-                ></v-img>
+                />
               </v-flex>
               <v-flex xs7>
                 <v-card-title primary-title>
                   <div>
-                    <div class="headline">{{ post.Title }}</div>
-                    <div>{{ post.PublishedAt }}</div>
+                    <div class="headline">
+                      {{ post.Title }}
+                    </div>
+                    <div>
+                      {{ post.PublishedAt }}
+                    </div>
                   </div>
                 </v-card-title>
               </v-flex>
@@ -31,27 +41,48 @@
           </v-card>
         </v-flex>
       </v-layout>
+      <v-paginate
+        ref="paginate"
+        :current-page="page"
+        :total-page="totalPage"
+        @click-page="fetchPosts"
+      />
     </v-flex>
   </v-layout>
 </template>
 
 <script>
 import Request from '~/assets/javascript/request.js'
+import Paginate from '~/components/paginate'
 
 export default {
+  components: {
+    'v-paginate': Paginate
+  },
   data: function() {
     return {
-      posts: []
+      posts: [],
+      totalPage: 0,
+      page: 1
+    }
+  },
+  watch: {
+    page: function() {
+      this.$refs.paginate.updateCurrent(this.page)
     }
   },
   mounted: function() {
     this.fetchPosts()
   },
   methods: {
-    fetchPosts: function() {
-      Request.get('/posts', {})
+    fetchPosts: function(page) {
+      const requestPage = page === undefined ? 1 : page
+      Request.get('/posts?page=' + requestPage, {})
         .then(response => {
+          // TODO: totalPage が応答されるようになったらそれをセットする
+          this.totalPage = 5
           this.posts = response.data.Posts
+          this.page = page
         })
         .catch(error => {
           console.log(error)
