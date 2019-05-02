90b48557ddb3525da511ac91c6e33de5fa60e790
naoki85 <naoki.yoneyama.85@gmail.com>
2019-05-02 22:18:12 +0900

    Fix ecosystem

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,30 +1,31 @@
 module.exports = {
-  // apps : [{
-  //   name: 'API',
-  //   script: 'app.js',
-
-  //   // Options reference: https://pm2.io/doc/en/runtime/reference/ecosystem-file/
-  //   args: 'one two',
-  //   instances: 1,
-  //   autorestart: true,
-  //   watch: false,
-  //   max_memory_restart: '1G',
-  //   env: {
-  //     NODE_ENV: 'development'
-  //   },
-  //   env_production: {
-  //     NODE_ENV: 'production'
-  //   }
-  // }],
-
-  deploy : {
-    production : {
-      user : 'naoki_yoneyama',
-      host : '13.231.79.158',
-      ref  : 'origin/master',
-      repo : 'git@github.com:naoki85/my_blog_nuxt.git',
-      path : '/home/naoki_yoneyama/my_blog_nuxt',
-      'post-deploy' : 'yarn install && yarn generate'
+  apps: [
+    {
+      name: 'my-blog-nuxt',
+      script: 'yarn run start',
+      // Options reference: https://pm2.io/doc/en/runtime/reference/ecosystem-file/
+      // args: 'run start',
+      instances: 1,
+      autorestart: true,
+      watch: false
+      // max_memory_restart: '1G'
+      // env: {
+      //   NODE_ENV: 'development'
+      // },
+      // env_production: {
+      //   NODE_ENV: 'production'
+      // }
+    }
+  ],
+  deploy: {
+    production: {
+      user: 'naoki_yoneyama',
+      host: '13.231.79.158',
+      ref: 'origin/master',
+      repo: 'git@github.com:naoki85/my_blog_nuxt.git',
+      path: '/home/naoki_yoneyama/my_blog_nuxt',
+      'post-deploy':
+        'yarn install && yarn build && yarn run pm2 startOrRestart ecosystem.config.js'
     }
   }
-};
+}
