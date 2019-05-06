c09e69b6e86c71edcf881265984b7a164eef7c7a
naoki85 <naoki.yoneyama.85@gmail.com>
2019-05-06 16:47:07 +0900

    Fix layouts/error

--- layouts/error.vue
+++ layouts/error.vue
@@ -8,8 +8,13 @@
     </h1>
   </div>
 </template>
-<script>
-export default {
-  props: ['error']
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator'
+
+@Component
+export default class Error extends Vue {
+  @Prop()
+  public error!: any
 }
 </script>
